ae64818a6b7caafa0525a6fedeffe9ddc7e3e9f2
alexforsale <alexforsale@yahoo.com>
2025-07-15 19:22:42 +0700

    plugins: use blink

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -40,17 +40,17 @@ return {
       require('mini.ai').setup { n_lines = 500 }
       -- Commenting with 'gcc'
       require('mini.comment').setup()
-      require('mini.completion').setup()
-      local gen_loader = require('mini.snippets').gen_loader
-      require('mini.snippets').setup({
-        snippets = {
-          -- Load custom file with global snippets first (adjust for Windows)
-          gen_loader.from_file('~/.config/nvim/snippets/global.json'),
-          -- Load snippets based on current language by reading files from
-          -- "snippets/" subdirectories from 'runtimepath' directories.
-          gen_loader.from_lang(),
-        },
-      })
+      -- require('mini.completion').setup()
+      -- local gen_loader = require('mini.snippets').gen_loader
+      -- require('mini.snippets').setup({
+      --   snippets = {
+      --     -- Load custom file with global snippets first (adjust for Windows)
+      --     gen_loader.from_file('~/.config/nvim/snippets/global.json'),
+      --     -- Load snippets based on current language by reading files from
+      --     -- "snippets/" subdirectories from 'runtimepath' directories.
+      --     gen_loader.from_lang(),
+      --   },
+      -- })
       -- mini-keymap
       local map_multistep = require('mini.keymap').map_multistep
 
