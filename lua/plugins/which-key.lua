5432a63a9f8fd26b2c3cb756f265b7e6d5f6ca8e
alexforsale <alexforsale@yahoo.com>
2025-07-16 00:10:24 +0700

    plugins/which-key: add spec

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,4 +1,10 @@
 return {
   'folke/which-key.nvim',
   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  opts = {
+    spec = {
+      { "<BS>", desc = "Decrement Selection", mode = "x" },
+      { "<c-space>", desc = "Increment Selection", mode = { "x", "n" } },
+    },
+  },
 }
