460db031ef267b17ece66c37927144026705fa5c
alexforsale <alexforsale@yahoo.com>
2025-07-15 12:46:39 +0700

    plugins: initial commits

---
+++ lua/plugins/guess-indent.lua
@@ -0,0 +1,21 @@
+return {
+  'NMAC427/guess-indent.nvim',
+  config = function()
+    require('guess-indent').setup {
+      auto_cmd = true,
+      filetype_exclude = {
+	"netrw",
+	"tutor",
+      },
+      on_tab_options = { -- A table of vim options when tabs are detected 
+	["expandtab"] = false,
+      },
+      on_space_options = { -- A table of vim options when spaces are detected 
+	["expandtab"] = true,
+	["tabstop"] = "detected", -- If the option value is 'detected', The value is set to the automatically detected indent size.
+	["softtabstop"] = "detected",
+	["shiftwidth"] = "detected",
+      },
+    }
+  end
+}
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,4 @@
+return {
+  'folke/which-key.nvim',
+  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+}
