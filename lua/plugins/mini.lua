bf1c7aee2aeffd16ddc8416eea34a2e049d96a20
alexforsale <alexforsale@yahoo.com>
2025-07-15 23:57:51 +0700

    plugins/mini: more mini.pairs setup

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -59,7 +59,21 @@ return {
       map_multistep('i', '<CR>',    { 'pmenu_accept', 'minipairs_cr' })
       map_multistep('i', '<BS>',    { 'minipairs_bs' })
       require('mini.move').setup()
-      require('mini.pairs').setup()
+      require('mini.pairs').setup {
+        event = "VeryLazy",
+        opts = {
+          modes = { insert = true, command = true, terminal = false },
+          -- skip autopair when next character is one of these
+          skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+          -- skip autopair when the cursor is inside these treesitter nodes
+          skip_ts = { "string" },
+          -- skip autopair when next character is closing pair
+          -- and there are more closing pairs than opening pairs
+          skip_unbalanced = true,
+          -- better deal with markdown code blocks
+          markdown = true,
+        },
+      }
       require('mini.move').setup()
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
       --
