7f737805d3fd865e0801e32fb286245af01710df
Kristian Alexander <alexforsale@madagascar.mkncorp.com>
2025-12-04 01:21:45 +0700

    updates

---
+++ lua/plugins/nvim-autopairs.lua
@@ -0,0 +1,8 @@
+return {
+  'windwp/nvim-autopairs',
+  event = "InsertEnter",
+  config = true,
+  opts = {
+    disable_in_replace_mode = true,
+  },
+}
--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,5 +2,24 @@ return {
   'folke/todo-comments.nvim',
   event = 'VimEnter',
   dependencies = { 'nvim-lua/plenary.nvim' },
-  opts = { signs = false },
+  opts = { 
+    signs = false,
+    keywords = {
+      FIX = {
+	icon = " ",
+	color = "error",
+	alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+      },
+      TODO = { icon = " ", color = "info" },
+      HACK = { icon = " ", color = "warning" },
+      WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+      PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+      NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+      TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+    },
+    keys = {
+      { "]t", "<cmd>lua require('todo-commments').jump_next()<CR>", mode = { "n", "o" } },
+      { "[t", "<cmd>lua require('todo-commments').jump_prev()<CR>", mode = { "n", "o" } },
+    },
+  },
 }
